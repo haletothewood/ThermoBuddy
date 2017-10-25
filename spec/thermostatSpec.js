46b011fc3554ed82af6151876c9f576b59f122cc
David Halewood <davidnhalewood@hotmail.com>
2017-10-25 15:38:09 +0100

    adding function to turn up the heat

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,12 +1,19 @@
-describe ('Thermostat', function() {
+'use strict';
+
+describe('Thermostat', function () {
 
   var thermostat;
 
-  beforeEach( function() {
+  beforeEach(function () {
     thermostat = new Thermostat();
   })
 
-  it('has a default temperature upon creation', function() {
+  it('has a default temperature upon creation', function () {
     expect(thermostat._temp).toEqual(20);
+  });
+
+  it('has a function to turn the temperature up', function () {
+    thermostat.turnUp()
+    expect(thermostat._temp).toEqual(21);
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,3 +1,9 @@
+'use strict';
+
 function Thermostat() {
   this._temp = 20
+};
+
+Thermostat.prototype.turnUp = function() {
+  this._temp += 1
 };
