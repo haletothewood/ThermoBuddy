2fe5632171d721b9677ad30b91d111f627927635
David Halewood <davidnhalewood@hotmail.com>
2017-10-27 09:55:44 +0100

    styling

--- src/interface.js
+++ src/interface.js
@@ -32,7 +32,7 @@ $(document).ready(function () {
 
   function updateTemperature() {
     $('#temp').text(thermostat.getTemp());
-    $('body').attr('class', thermostat.energyUsage());
+    $('#temp-box').attr('class', thermostat.energyUsage());
   }
 
   $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
